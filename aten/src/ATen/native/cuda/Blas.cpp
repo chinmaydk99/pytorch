bc3eb04a416f33f07f157b937df309c0ec0dffd4
vuksan314 <vmijovic@amd.com>
2025-11-10 14:42:43 +0100

    [release/2.9] Add gfx110X and gfx115X to prefered hipBLASLt list (#2742)
    hipblaslt should provide better performance in general

--- aten/src/ATen/native/cuda/Blas.cpp
+++ aten/src/ATen/native/cuda/Blas.cpp
@@ -123,10 +123,13 @@ static bool isGloballyDisabledAddmmCudaLt(const at::Device& device) {
   static const std::vector<std::string> archs = {
         "gfx90a", "gfx942",
     #if ROCM_VERSION >= 60300
-        "gfx1100", "gfx1101", "gfx1200", "gfx1201", "gfx908",
+        "gfx1100", "gfx1101", "gfx1102", "gfx1200", "gfx1201", "gfx908",
     #endif
-    #if ROCM_VERSION >= 70000
-        "gfx950", "gfx1150", "gfx1151"
+    #if ROCM_VERSION >= 60402
+        "gfx1150", "gfx1151",
+    #endif
+    #if ROCM_VERSION >= 60500
+        "gfx950"
     #endif
   };
   const auto is_hipblas_lt_arch_supported = at::detail::getCUDAHooks().isGPUArch(archs, device.index());
